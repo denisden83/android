4de844621af60d418378928539b1024dfa81c230
denys <denisden83@gmail.com>
2022-02-03 05:40:10 +0100

    navigation to AddItemActivity

--- app/src/main/java/com/example/money/MainActivity.java
+++ app/src/main/java/com/example/money/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.money;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.welcome_sign).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newActivity = new Intent(getApplicationContext(), AddItemActivity.class);
+                startActivity(newActivity);
+            }
+        });
     }
 }
